ac2f80595d194d79713b2c38525133f50af3e7a8
Jesse Lentz <jesselentz@gmail.com>
2017-05-16 00:48:19 -0400

    Fix Mocha output colors for Solarized

--- test/support/index.js
+++ test/support/index.js
@@ -1,1 +1,7 @@
 global.assert = require('assert');
+
+// Fix Mocha colors for Solarized
+const colors = require('mocha/lib/reporters/base').colors
+colors['pass'] = '92';
+colors['error message'] = '37;41';
+colors['error stack'] = '0';

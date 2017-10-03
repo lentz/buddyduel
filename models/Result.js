7e556fff7c8a822453a412ab02b1af560df41ee2
Jesse Lentz <jesselentz@gmail.com>
2017-10-03 19:41:19 -0400

    Remove _id field from Result score objects

--- models/Result.js
+++ models/Result.js
@@ -4,6 +4,7 @@ const resultSchema = new mongoose.Schema({
   year: { type: Number, required: true },
   weekNum: { type: Number, required: true },
   scores: [{
+    _id: false,
     gameId: { type: String, required: true },
     homeScore: { type: Number, required: true },
     awayScore: { type: Number, required: true },

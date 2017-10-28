979e6ab14f81cf4caac343bface2d2518ef0054a
Jesse Lentz <jesselentz@gmail.com>
2017-10-28 13:18:33 -0400

    Refactor users controller to use async parallel

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,33 @@
 /* eslint no-param-reassign: 0 */
 
+const async = require('async');
 const DuelWeek = require('../models/DuelWeek');
 const error = require('../lib/error');
 
-module.exports.show = (req, res) => {
-  const profile = { record: { wins: 0, losses: 0, pushes: 0 }, winnings: 0 };
-  DuelWeek.find({ 'picker.id': req.user.sub }, { record: 1, winnings: 1 },
+function getPerformance(userId, cb) {
+  DuelWeek.find({ 'picker.id': userId }, { record: 1, winnings: 1 },
     (err, duelWeeks) => {
-      if (err) { return error.send(res, err, 'Failed to get user record'); }
-      duelWeeks.reduce((currentProfile, duelWeek) => {
-        currentProfile.winnings += duelWeek.winnings;
-        currentProfile.record.wins += duelWeek.record.wins;
-        currentProfile.record.losses += duelWeek.record.losses;
-        currentProfile.record.pushes += duelWeek.record.pushes;
-        return currentProfile;
-      }, profile);
-      return res.json(profile);
+      if (err) { return cb(err); }
+      return cb(null, duelWeeks.reduce((performance, duelWeek) => {
+        performance.winnings += duelWeek.winnings;
+        performance.record.wins += duelWeek.record.wins;
+        performance.record.losses += duelWeek.record.losses;
+        performance.record.pushes += duelWeek.record.pushes;
+        return performance;
+      }, { winnings: 0, record: { wins: 0, losses: 0, pushes: 0 } }));
     });
+}
+
+function getPreferences(userId, cb) {
+  return cb(null, { preferences: {} });
+}
+
+module.exports.show = (req, res) => {
+  async.parallel({
+    performance: async.apply(getPerformance, req.user.sub),
+    preferences: async.apply(getPreferences, req.user.sub),
+  }, (err, results) => {
+    if (err) { return error.send(res, err, 'Failed to get user profile'); }
+    return res.json(Object.assign(results.performance, results.preferences));
+  });
 };

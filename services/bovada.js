e23cf89e2922dbc7e8670853a75ac58363c35da4
Jesse Lentz <jesselentz@gmail.com>
2019-01-02 22:52:46 -0500

    Support playoff lines at different Bovada URL

--- services/bovada.js
+++ services/bovada.js
@@ -1,11 +1,15 @@
 const axios = require('axios');
 const BovadaParser = require('../services/BovadaParser');
 
-module.exports.getLines = async () => BovadaParser.call((await axios.get(
-  'https://www.bovada.lv/services/sports/event/v2/events/A/description/football/nfl?marketFilterId=def&preMatchOnly=true&lang=en',
-  {
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36',
-    },
-  }
-)).data);
+axios.defaults.baseURL = 'https://www.bovada.lv/services/sports/event/v2/events/A/description/football';
+axios.defaults.headers.common['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36';
+
+module.exports.getLines = async () => {
+  const [regSeasonResp, playoffResp] = await Promise.all([
+    axios.get('/nfl?marketFilterId=def&preMatchOnly=true&lang=en'),
+    axios.get('/nfl-playoffs?marketFilterId=def&preMatchOnly=true&lang=en'),
+  ]);
+
+  return BovadaParser.call(regSeasonResp.data)
+    .concat(BovadaParser.call(playoffResp.data));
+};

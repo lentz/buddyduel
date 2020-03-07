90c75772cdc8e22ab79a242d2097ed92ed37967d
Jesse Lentz <jesselentz@gmail.com>
2020-03-06 22:33:36 -0500

    Rename benchmark var

--- api/src/bin/update-duel-weeks.ts
+++ api/src/bin/update-duel-weeks.ts
@@ -5,7 +5,7 @@ import { default as Duel, IDuel } from '../models/Duel';
 import * as DuelWeekUpdater from '../services/DuelWeekUpdater';
 
 async function run() {
-  const startTime = Date.now();
+  const beginTime = Date.now();
   try {
     const duels = await Duel.find({ status: 'active' }).exec() as IDuel[];
     await DuelWeekUpdater.call(duels);
@@ -13,7 +13,7 @@ async function run() {
     console.error('Error updating duel weeks:', err);
   } finally {
     db.close();
-    console.log('Completed in', Date.now() - startTime, 'ms');
+    console.log('Completed in', Date.now() - beginTime, 'ms');
     process.exit();
   }
 }

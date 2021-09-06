1ad7f70436fd94818fb45764478802de3816acce
Jesse Lentz <jesselentz@gmail.com>
2021-09-06 12:52:49 -0400

    Update Mongoose options for v6

--- api/src/lib/db.ts
+++ api/src/lib/db.ts
@@ -4,9 +4,7 @@ import logger from './logger';
 
 dotenv.config();
 
-mongoose.set('useFindAndModify', false);
-mongoose.set('useUnifiedTopology', true);
-mongoose.connect(process.env.MONGODB_URI || '', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI ?? '');
 mongoose.connection.on('error', (err) => {
   logger.error(`Unable to connect to Mongo: ${err}`);
   process.exit(1);

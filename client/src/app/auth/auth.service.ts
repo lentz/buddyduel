80f1f0ff0b7b73ed24107f4359ad3cacda2d9441
Jesse Lentz <jesselentz@gmail.com>
2022-02-14 10:18:42 -0500

    Update to Angular 12

--- client/src/app/auth/auth.service.ts
+++ client/src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import * as Cookies from 'js-cookie';
+import Cookies from 'js-cookie';
 import { environment } from '../../environments/environment';
 
 @Injectable()

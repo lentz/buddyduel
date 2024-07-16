ac0b51109a4eb509373a5c9716eb06a798670a93
Jesse Lentz <jesselentz@gmail.com>
2024-07-16 19:58:05 -0400

    Format code

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,7 @@
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -20,18 +23,28 @@ export function getToken() {
   return localStorage.getItem('access_token');
 }
 
-@NgModule({ declarations: [
-        AppComponent,
-        HomeComponent,
-        NavComponent,
-        UserProfileComponent,
-    ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        BrowserAnimationsModule,
-        DuelsModule,
-        DuelWeeksModule,
-        FormsModule,
-        ToastrModule.forRoot(),
-        SharedModule,
-        AppRoutingModule], providers: [AuthService, UserProfileService, provideHttpClient(withInterceptorsFromDi())] })
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    UserProfileComponent,
+  ],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    DuelsModule,
+    DuelWeeksModule,
+    FormsModule,
+    ToastrModule.forRoot(),
+    SharedModule,
+    AppRoutingModule,
+  ],
+  providers: [
+    AuthService,
+    UserProfileService,
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
+})
 export class AppModule {}
